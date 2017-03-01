8d2f22add9ce7004a5ef71c43c2c867f71c2c2c2
Brittain Hard <brittainhard@gmail.com>
2017-03-01 12:54:47 -0600

    List-struct example.

---
+++ c/samples/lists_of_structs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Person {
+    char *name;
+    int age;
+} Person;
+
+
+int main(int argc, char *argv[])
+{
+    Person *kelly = malloc(sizeof(Person));
+    Person **persons = malloc(sizeof(Person) * 10);
+    kelly -> name = "Kelly";
+    kelly -> age = 27;
+    persons[0] = kelly;
+    printf("%d\n", persons[0]->age);
+    free(kelly);
+    free(persons);
+    return 0;
+}
+
