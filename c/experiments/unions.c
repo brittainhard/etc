d398647148ab15946fa626212d36e245b8f0a3ac
Brittain Hard <brittainhard@gmail.com>
2017-05-07 13:52:24 -0500

    Notes on the output of the unions program.

--- c/experiments/unions.c
+++ c/experiments/unions.c
@@ -21,17 +21,30 @@ void struct_nonsense()
     free(one);
 }
 
+/**
+ * Unions can only access one value at a time, so when you try to access the
+ * value of a union attribute after another attribute has been set, you will get
+ * back a garbage value.
+ *
+ * You need to be thankful for what you have.
+ */
 void union_nonsense()
 {
-    int i = 0;
     y *two = malloc(sizeof(x));
     two->first = 1;
     two->second = 2.0;
-    i = i + two->first;
+
+    // Prints arbitrary value because two -> second has been set.
     printf("%d\n", two->first);
+
+    // Prints second value correctly, 2.0.
     printf("%f\n", two->second);
-    printf("%d\n", i);
     free(two);
+
+    /**
+     * The goal is not to get somewhere, and get there fast; not to be something
+     * and become it quickly. By doing the thing, you are the thing.
+     */
 }
 
 int main(int argc, char *argv[])
