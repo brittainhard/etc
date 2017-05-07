e64481fdc32e88140c93532e0b47569c99acd789
Brittain Hard <brittainhard@gmail.com>
2017-05-07 12:03:17 -0500

    Sample hashtable, to be used for ledger program.

---
+++ c/programs/ledger.c
@@ -0,0 +1,18 @@
+/**
+ * Can we just do this by sorting by date? I think we can sort this by date.
+ * Dates are a special mix of different int bases.
+ *
+ * Two arguments, read and write.
+ */
+#include <stdio.h>
+
+
+struct LedgerItem {
+    int date;
+} LedgerItem;
+
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
