97f8a9dadeb4a13fcdb2ff787812b1b610f17a21
Brittain Hard <brittainhard@gmail.com>
2017-05-25 10:44:37 -0500

    Added cython files.

---
+++ c/scanf_experiments.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+    char input[265];
+    fgets(input, 256, "Hey!");
+    return 0;
+}
---
+++ scratch.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main(int argc, char *argv[]) {
+    int a;
+    bool b;
+    printf("%d %d\n", a, b);
+    return 0;
+}
