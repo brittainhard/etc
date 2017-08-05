632d1256fbd57f06beeba22075076c7ccacb39b1
Brittain Hard <brittainhard@gmail.com>
2017-08-05 13:34:17 -0500

    Note on bit fields.

--- c/experiments/bit_fields.c
+++ c/experiments/bit_fields.c
@@ -1,7 +1,11 @@
 /**
  * So you can save some memory using bit fields as statuses, right? If you
  * malloc them, it seems like you get the same shit. It expands the size of the
- * object to be something else entirely
+ * object to be something else entirely.
+ *
+ * The bit fields let you declare some variables while keeping a very small size.
+ *
+ * This only works with ints apparently. This is only useful.
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,19 +34,22 @@ struct {
 
 void printing_nonsense()
 {
-    struct is_apple *apple = malloc(sizeof(is_apple));    
-    struct is_potato *potato = malloc(sizeof(is_potato));    
+    struct is_apple *apple = malloc(sizeof(is_apple));
+    struct is_potato *potato = malloc(sizeof(is_potato));
     printf("%lu\n", sizeof(apple));
     printf("%lu\n", sizeof(potato));
-    printf( "Memory size occupied by status1 : %d\n", sizeof(status1));
-    printf( "Memory size occupied by status2 : %d\n", sizeof(status2));
+    printf( "Memory size occupied by status1 : %lu\n", sizeof(status1));
+    printf( "Memory size occupied by status2 : %lu\n", sizeof(status2));
     printf("%lu\n", sizeof(is_apple));
     printf("%lu\n", sizeof(is_potato));
+    printf("%lu\n", sizeof(is_potato.potato));
+    printf("%lu\n", sizeof(is_apple.potato));
     free(apple);
     free(potato);
 }
 
 int main(int argc, char *argv[])
 {
+    printing_nonsense();
     return 0;
 }
