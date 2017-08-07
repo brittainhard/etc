3ebf3c3d26c6713660806bb6bd57026d8d25dd55
Brittain Hard <brittainhard@gmail.com>
2017-08-06 19:46:07 -0500

    Updates to repr_str file.

--- book_notes/mastering_oop/chapter2/repr_and_str.py
+++ book_notes/mastering_oop/chapter2/repr_and_str.py
@@ -1,4 +1,5 @@
 class Potato:
+
     def __init__(self, potato, apple):
         self.potato = potato
         self.apple = apple
@@ -6,11 +7,29 @@ class Potato:
     def __repr__(self):
         """This is what shows up when you try to investigate what something is,
         for example, when you are debugging."""
-        return "%s (apple=%s, potato=%s)" % (self.__class__.__name__, self.potato, self.apple)
+        repr_string = "{__class__.__name__}(potato={potato!s}, apple={apple!s})"
+        return repr_string.format(__class__=self.__class__, **self.__dict__)
 
     def __str__(self):
         """This is what shows up when you print something."""
-        return "Potato"
+        return "{potato} {apple}".format(**self.__dict__)
 
-potato = Potato(1, 2)
-print(potato)
+
+class PotatoSack:
+
+    def __init__(self, potatoes):
+        self.potatoes = list(potatoes)
+
+    def __str__(self):
+        return ", ".join(map(str, self.potatoes))
+
+    def __repr__(self):
+        repr_string = "{__class__.__name__}({_potato_str})"
+        return repr_string.format(
+            __class__ = self.__class__,
+            _potato_str=",".join(map(repr, self.potatoes)),
+        )
+
+content = "For a long time I used to go to bed early."
+# Pretty sure this calls the split function twice. Not he best.
+potatoes = PotatoSack(list(map(Potato, content.split(" "), range(len(content.split(" "))))))
